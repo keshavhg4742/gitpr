41eb1b23ffe7dade053e9dc0fa430809c336e543
keshavhg4742 <45967286+keshavhg4742@users.noreply.github.com>
2025-05-06 23:08:51 +0530

    Create Cd.java

---
+++ src/test/java/gitpract/Cd.java
@@ -0,0 +1,12 @@
+public class Cd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("pull req");
+
+		System.out.println(" req");
+
+
+	}
+
+}
