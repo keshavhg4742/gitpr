235871cc84b999eaffb01be9ca1c5cedd161403a
keshavhg4742 <45967286+keshavhg4742@users.noreply.github.com>
2025-05-06 22:54:39 +0530

    Update Dumm.java

--- src/test/java/gitpract/Dumm.java
+++ src/test/java/gitpract/Dumm.java
@@ -4,7 +4,8 @@ public class Dumm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(pull req");
+		System.out.println("pull req");
+		System.out.println(" req");
 
 	}
 
